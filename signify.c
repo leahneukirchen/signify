d25691844edb636d4110febf34676e927d901c09
jmc <jmc>
2013-12-31 17:33:17 +0000

    various tweaks; ok tedu

--- signify.c
+++ signify.c
@@ -64,8 +64,8 @@ extern char *__progname;
 static void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [-N] [-I input] [-P pubkey] [-S seckey] "
-	    "-V generate|sign|verify\n", __progname);
+	fprintf(stderr, "usage: %s [-N] [-I input] [-O output] [-P pubkey] [-S seckey] "
+	    "-V generate | sign | verify\n", __progname);
 	exit(1);
 }
 
