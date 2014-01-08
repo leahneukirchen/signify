e60ad75a3c12c7f15d53e223775a6653d63ad094
tedu <tedu>
2014-01-08 03:59:46 +0000

    reduce size by using equivalent code from libc instead of nacl ok deraadt

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CPPFLAGS += -I${.CURDIR}/../ssh
 
 SRCS=	signify.c
 SRCS+=	ed25519.c fe25519.c ge25519.c sc25519.c smult_curve25519_ref.c
-SRCS+=	blocks.c hash.c verify.c
+SRCS+=	crypto_api.c
 
 PROG=	signify
 
---
+++ crypto_api.c
@@ -0,0 +1,29 @@
+/* $OpenBSD$ */
+/*
+ * Public domain. Author: Ted Unangst <tedu@openbsd.org>
+ * API compatible reimplementation of functions from nacl
+ */
+#include <sys/types.h>
+
+#include <string.h>
+#include <sha2.h>
+
+#include "crypto_api.h"
+
+int
+crypto_hash_sha512(unsigned char *out, const unsigned char *in,
+    unsigned long long inlen)
+{
+	SHA2_CTX ctx;
+
+	SHA512Init(&ctx);
+	SHA512Update(&ctx, in, inlen);
+	SHA512Final(out, &ctx);
+	return 0;
+}
+
+int
+crypto_verify_32(const unsigned char *x, const unsigned char *y)
+{
+	return timingsafe_bcmp(x, y, 32) ? -1 : 0;
+}
