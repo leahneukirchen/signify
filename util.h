11278ecdfb2b9ac6fbec4c2408620bbdc1c46462
Christian Neukirchen <chneukirchen@gmail.com>
2014-04-23 13:34:10 +0200

    make util.h nicer

--- util.h
+++ util.h
@@ -1,27 +1,13 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-     size_t
-     strlcpy(char *dst, const char *src, size_t size);
-
-       void explicit_bzero(void *s, size_t n);
-
-int
-timingsafe_bcmp(const void *b1, const void *b2, size_t n);
-
-
-void
-arc4random_buf(void *buf, size_t n);
-
-int	bcrypt_pbkdf(const char *, size_t, const uint8_t *, size_t,
-    uint8_t *, size_t, unsigned int);
-
-int RAND_bytes(unsigned char *buf, int num);
-
-int
-b64_ntop(u_char const *src, size_t srclength, char *target, size_t targsize);
-int
-  b64_pton(char const *src, u_char *target, size_t targsize);
-
-void * reallocarray(void *optr, size_t nmemb, size_t size);
+size_t	strlcpy(char *, const char *, size_t);
+void	explicit_bzero(void *, size_t);
+int	timingsafe_bcmp(const void *, const void *, size_t);
+void	arc4random_buf(void *, size_t);
+int	bcrypt_pbkdf(const char *, size_t, const uint8_t *, size_t, uint8_t *, size_t, unsigned int);
+int	RAND_bytes(unsigned char *, int);
+int	b64_ntop(u_char const *, size_t, char *, size_t);
+int	b64_pton(char const *, u_char *target, size_t);
+void *	reallocarray(void *, size_t, size_t);
 
